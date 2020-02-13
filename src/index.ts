307d073931503e6acd4eb4fa8dc5d5acbd8c8bac
Elvis339 <elvissabanovic3@gmail.com>
2020-02-13 18:37:43 +0100

    updated package.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { Client } from './Client/Client';
 
-module.exports = {
-    Slave: Client
-}
+/**
+ * Example
+ * Using it for TS project -> Middleware or Services
+ */
+(async function() {
+    try {
+        // Init Cloud
+        let client = new Client('email', 'password', 'client-name', 'client-vendor', 'https://app.activecollab.com', 123456)
+        let token = await client.issueToken()
+        let data = await client._get('projects')
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+})()
